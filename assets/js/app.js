0475f09f523c54f8be46b8f9457cd62aa34a08cd
Brent Thomas <brentmt24@gmail.com>
2015-07-21 08:56:48 -0500

    working on buttons

---
+++ assets/js/app.js
@@ -0,0 +1,28 @@
+(function(){
+  var listButtonEl = document.getElementsByClassName(beer);
+  var gridButtonEl = document.getElementsByClassName(beer);
+
+  listButtonEl.onclick = function list(beerClass) {
+    listButtonEl.classList.remove("grid");
+    listButtonEl.classList.add("list");
+  };
+
+  listButtonEl.onclick = function grid(beerClass) {
+    listButtonEl.classList.remove("list");
+    listButtonEl.classList.add("grid");
+
+  };
+
+  var templateFromQuery = function(query) {
+    var el = document.querySelector(query);
+    return Handlebars.compile(el.innerHTML);
+  };
+
+  var template = templateFromQuery('.beer-item');
+
+
+  beerData.data.forEach(function(beer) {
+    var addBeer = template(beer);
+    document.querySelector('body').innerHTML += addBeer;
+  });
+})();
